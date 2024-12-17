ad703cffb3ccbc29f830570ef7e3ee9a59367137
JackCarbz <s8901812k@majorana-giorgi.edu.it>
2024-12-17 09:35:42 +0100

    TutteLeOperazioni

--- src/main/java/Calcolatrixce.java
+++ src/main/java/Calcolatrixce.java
@@ -10,4 +10,35 @@ public class Calcolatrixce
 		}
 		return s;
 	}
+	
+	public double sottrazione(double...addendi)
+	{
+		double s = addendi[0];
+		for(int i = 1; i<addendi.length; i++)
+		{
+			s = s - addendi[i];
+		}
+		return s;
+	}
+	
+	public double moltiplicazione(double...addendi)
+	{
+		double s = 1;
+		for(double addendo : addendi)
+		{
+			s = s * addendo;
+		}
+		return s;
+	}
+	public double divisione(double...addendi) throws ArithmeticException
+	{
+		double s = addendi[0];
+		for(int i = 1; i<addendi.length; i++)
+		{
+			if(addendi[i] == 0)
+				throw new ArithmeticException();
+			s = s / addendi[i];
+		}
+		return s;
+	}
 }
--- src/test/java/TestCalcolatrice.java
+++ src/test/java/TestCalcolatrice.java
@@ -31,6 +31,41 @@ class TestCalcolatrice {
 		
 		double res = c.somma(10,-5);
 		
-		assertEquals(15,res,0);
+		assertEquals(5,res,0);
+	}
+	
+	@Test
+	public void testSottra()
+	{
+		Calcolatrixce c = new Calcolatrixce();
+		
+		double res = c.sottrazione(10,5);
+		
+		assertEquals(5,res,0);
+	}
+	
+	@Test
+	public void testMolti()
+	{
+		Calcolatrixce c = new Calcolatrixce();
+		
+		double res = c.moltiplicazione(3, 4, 2);
+		
+		assertEquals(24,res,0);
+	}
+	
+	@Test
+	public void testDivi()
+	{
+		Calcolatrixce c = new Calcolatrixce();
+		try
+		{
+			double res = c.divisione(12, 4);
+			assertEquals(3,res,0);
+		}
+		catch(ArithmeticException eccezione)
+		{
+			System.out.println("errore, eccezione lanciata");
+		}
 	}
 }
