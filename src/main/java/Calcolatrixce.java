d6493080419a87ad272ab6ce267b569ec411a0cd
JackCarbz <s8901812k@majorana-giorgi.edu.it>
2024-12-17 08:49:47 +0100

    ModificaInClasse

--- src/main/java/Calcolatrixce.java
+++ src/main/java/Calcolatrixce.java
@@ -6,7 +6,7 @@ public class Calcolatrixce
 		double s = 0;
 		for(double addendo : addendi)
 		{
-			s = s + Math.abs(addendo);
+			s = s + addendo;
 		}
 		return s;
 	}
--- src/test/java/TestCalcolatrice.java
+++ src/test/java/TestCalcolatrice.java
@@ -1,6 +1,5 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 class TestCalcolatrice {
@@ -12,7 +11,7 @@ class TestCalcolatrice {
 		
 		double res = c.somma(5,6);
 		
-		Assert.assertEquals(11,res,0);
+		assertEquals(11,res,0);
 	}
 	
 	@Test
@@ -22,7 +21,7 @@ class TestCalcolatrice {
 		
 		double res = c.somma(6,6);
 		
-		Assert.assertEquals(12,res,0);
+		assertEquals(12,res,0);
 	}
 	
 	@Test
@@ -32,6 +31,6 @@ class TestCalcolatrice {
 		
 		double res = c.somma(10,-5);
 		
-		Assert.assertEquals(5,res,0);
+		assertEquals(5,res,0);
 	}
 }
